b06c676da5cde63fd5c7b4ce7dbddebd95d349a2
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-02-03 22:54:06 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -1357,3 +1357,47 @@ public class MyClass
 
 Output-
 	1 1 1 3 4
+	
+	
+29)Least of Subarray
+
+public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		int[] a= 
+		{
+			3,7,90,20,5,50,40
+		}
+		;
+		int k=3;
+		myCode(a,k);
+	}
+	//EndOfMainMethod
+	private static void myCode(int[] a,int k) 
+	{
+		//WriteCode Here
+		int min=Integer.MAX_VALUE;
+		int index=0;
+		for(int i=0;i<a.length-k+1;i++)
+		{
+			int sum=0;
+			for(int j=i;j<i+k;j++)
+			{
+				sum=sum+a[j];
+			}
+			int avg=sum/k;
+			if(avg<min)
+			{
+				min=avg;
+				index=i;
+			}
+		}
+		System.out.println(min+" "+index);
+	}
+}
+
+
+Output-
+25 3
