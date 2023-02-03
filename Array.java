ea4b3e9079d4962d09e588201e0d4411d6745530
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-02-03 11:22:09 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -1044,4 +1044,74 @@ public class euix {
 
 Output:-
 not same
+
+
+
+24)Unique Pair 
+
+
+public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		int[] a= 
+		{
+			1,2,3,2,3
+		}
+		;
+		myCode(a);
+	}
+	//EndOfMainMethod
+	private static void myCode(int[] a) 
+	{
+		//WriteCode Here
+		int cnt=0;
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[i]==a[j])
+				{
+					cnt++;
+				}
+			}
+		}
+		int[]b=new int[a.length-cnt];
+		int index=0;
+		//int cnt1;
+		for(int i=0;i<a.length;i++)
+		{
+			int cnt1=0;
+			for(int j=0;j<i;j++)
+			{
+				if(a[i]==a[j])
+				{
+					cnt1++;
+				}
+			}
+			if(cnt1==0)
+			b[index++]=a[i];
+		}
+		for(int i=0;i<b.length;i++)
+		{
+			for(int j=0;j<b.length;j++)
+			{
+				System.out.println(b[i]+ " "+b[j]);
+			}
+		}
+	}
+}
+
+Output-
+1 1
+1 2
+1 3
+2 1
+2 2
+2 3
+3 1
+3 2
+3 3
+
  
