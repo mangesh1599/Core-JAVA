e216f61f0aed9f7e9889a12c18d935993242e62b
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-02-03 14:40:07 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -1114,4 +1114,53 @@ Output-
 3 2
 3 3
 
+	
+-------------------------------------------------------------------------------------------------------------------------------------------
+	
+25)Remove Specific Element from Array
+
+public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		int[] a= 
+		{
+			33,7,90,20,5,50,40
+		}
+		;
+		int ele=78;
+		myCode(a,ele);
+	}
+	//EndOfMainMethod
+	private static void myCode(int[] a,int ele) 
+	{
+		//WriteCode Here
+		int cnt=0;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]==ele)
+			{
+				cnt++;
+			}
+		}
+		int []b=new int[a.length-cnt];
+		int index=0;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]!=ele)
+			{
+				b[index++]=a[i];
+			}
+		}
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(b[i]+" ");
+		}
+	}
+}
+
+Output:-
+	37 7 90 20 5 50 40
+	
  
