27062ce7068f278c31f8fa3424f2d9eb783b1199
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-02-07 16:02:26 +0530

    Update String.java

--- String.java
+++ String.java
@@ -206,3 +206,211 @@ Output-
 a 2
 b 2
 c 2
+	
+-----------------------------------------------------------------------------------------------------------------------	
+	
+
+7)Index Of All The Characters Of The Alphabet
+
+
+public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		String s1 ="abcd";
+		myCode(s1);
+	}
+	//EndOfMainMethod
+	private static void myCode(String s1) 
+	{
+		//write code here
+		for(int i=0;i<s1.length();i++)
+		{
+			char ch=s1.charAt(i);
+			System.out.println(i+" "+ch);
+		}
+	}
+}
+
+Output-
+0 a
+1 b
+2 c
+3 d
+
+
+--------------------------------------------------------------------------------------------------------------------------
+
+8)remove A Specified Character From AString
+
+public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		String s1="Aabcd";
+		char ch='a';
+		myCode(s1,ch);
+	}
+	//EndOfMainMethod
+	private static void myCode(String s1,char ch) 
+	{
+		//WriteCode Here
+		for(int i=0;i<s1.length();i++)
+		{
+			if(s1.charAt(i)!=ch)
+			{
+				System.out.print(s1.charAt(i));
+			}
+		}
+	}
+}
+
+Output-
+Abcd
+	
+------------------------------------------------------------------------------------------------------------------------------
+	
+
+9)	sum Of The Digits Present In The Given String
+
+public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		String s1="a2ba1ca3d";
+		myCode(s1);
+	}
+	//EndOfMainMethod
+	private static void myCode(String s1) 
+	{
+		//WriteCode Here
+		int sum=0;
+		for(int i=0;i<s1.length();i++)
+		{
+			char ch=s1.charAt(i);
+			if(ch>'0' && ch<'9')
+			sum=sum+ch-48;
+		}
+		System.out.println(sum);
+	}
+}
+
+Output-
+6
+	
+--------------------------------------------------------------------------------------------------------------------------------------	
+	
+
+10)String Convert To Upper Case
+
+
+import java.util.Arrays;
+import java.util.Iterator;
+public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		String s1 = "welcome";
+		myCode(s1);
+	}
+	//EndOfMainMethod
+	private static void myCode(String s1) 
+	{
+		//WriteCode Here
+		String s2="";
+		for(int i=0;i<s1.length();i++)
+		{
+			char ch=s1.charAt(i);
+			if(ch>='a' && ch<='z')
+			{
+				ch=(char)(ch-32);
+			}
+			s2=s2+ch;
+		}
+		System.out.println(s2);
+	}
+}
+
+Output-
+WELCOME
+
+	
+----------------------------------------------------------------------------------------------------------------------------------------
+
+
+11)String Convert To LowerCase
+
+import java.util.Arrays;
+import java.util.Iterator;
+public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		String s1 = "WELCOME";
+		myCode(s1);
+	}
+	//EndOfMainMethod
+	private static void myCode(String s1) 
+	{
+		//WriteCode Here
+		String s2="";
+		for(int i=0;i<s1.length();i++)
+		{
+			char ch=s1.charAt(i);
+			if(ch>='A' && ch<='Z')
+			{
+				ch=(char)(ch+32);
+			}
+			s2=s2+ch;
+		}
+		System.out.println(s2);
+	}
+}
+
+Output-
+welcome
+	
+-------------------------------------------------------------------------------------------------------------------------------	
+	
+
+12)remove Duplicate Ele From String
+
+import java.util.Arrays;
+import java.util.Iterator;
+public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		String s1 = "abbcfedafbcd";
+		myCode(s1);
+	}
+	//EndOfMainMethod
+	private static void myCode(String s1) 
+	{
+		//write code here
+		for(int i=0;i<s1.length();i++)
+		{
+			int cnt=0;
+			for(int j=0;j<i;j++)
+			{
+				if(s1.charAt(i)==s1.charAt(j))
+				{
+					cnt++;
+				}
+			}
+			if(cnt==0)
+			{
+				System.out.print(s1.charAt(i));
+			}
+		}
+	}
+}
+
+Output-
+abcfed
