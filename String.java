ed88db3f45b21e9a1f0b6e52a83fd53a9badbae6
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-02-08 15:47:38 +0530

    Update String.java

--- String.java
+++ String.java
@@ -560,3 +560,51 @@ public class MyClass
 
 Output-
 	to hef welcome
+	
+	
+	
+	
+16)compare A Given String To The Specified character Sequence
+	
+	
+	
+	public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		String s1="abcd";
+		String s2="bacd";
+		myCode(s1,s2);
+	}
+	//EndOfMainMethod
+	private static void myCode(String s1,String s2) 
+	{
+		//WriteCode Here
+		int cnt=0;
+		if(s1.length()==s2.length())
+		{
+			for(int i=0;i<s1.length();i++)
+			{
+				if(s1.charAt(i)!=s2.charAt(i))
+				{
+					cnt++;
+					System.out.println(false);
+					break;
+				}
+			}
+			if(cnt==0)
+			{
+				System.out.println(true);
+			}
+		}
+		else
+		{
+			System.out.println(false);
+		}
+	}
+}
+			
+			
+Output-
+	false
