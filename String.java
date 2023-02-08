728410d3488b9ed5b825be7cfa774732b1b1a209
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-02-08 15:28:30 +0530

    Update String.java

--- String.java
+++ String.java
@@ -516,3 +516,47 @@ public class MyClass
 
 Outpur-
 	12345
+	
+	
+	
+	
+15)String Array Sort By Length
+
+	
+import java.util.Arrays;
+import java.util.Iterator;
+public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		String s1 = "welcome to hef";
+		myCode(s1);
+	}
+	//EndOfMainMethod
+	private static void myCode(String s1) 
+	{
+		//WriteCode Here
+		String []c=s1.split(" ");
+		for(int i=0;i<c.length;i++)
+		{
+			for(int j=i+1;j<c.length;j++)
+			{
+				if(c[i].length()>c[j].length())
+				{
+					String temp=c[i];
+					c[i]=c[j];
+					c[j]=temp;
+				}
+			}
+		}
+		for(int i=0;i<c.length;i++)
+		{
+			System.out.print(c[i]+" ");
+		}
+	}
+}
+			
+
+Output-
+	to hef welcome
